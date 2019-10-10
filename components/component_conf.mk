4bc110cf593c6e96db990970d5924f8243026338
Sun Xiang Yu <sunxiangyu@espressif.com>
2019-10-10 14:18:53 +0800

    modify example Makefile

--- components/component_conf.mk
+++
@@ -1,6 +0,0 @@
-EXTRA_COMPONENT_DIRS += $(SOLUTION_PATH)/components
-EXTRA_COMPONENT_DIRS += $(SOLUTION_PATH)/components/hardware_driver
-EXTRA_COMPONENT_DIRS += $(SOLUTION_PATH)/components/esp-sr/acoustic_algorithm
-EXTRA_COMPONENT_DIRS += $(SOLUTION_PATH)/components/esp-sr/lib
-EXTRA_COMPONENT_DIRS += $(SOLUTION_PATH)/components/esp-sr/speech_command_recognition
-EXTRA_COMPONENT_DIRS += $(SOLUTION_PATH)/components/esp-sr/wake_word_engine
---
+++ components/hardware_driver/component.mk
@@ -0,0 +1,16 @@
+COMPONENT_ADD_INCLUDEDIRS := MediaHal \
+                             MediaHal/Board \
+                             MediaHal/Codec \
+                             SystemSal \
+                             i2c_bus \
+                             userconfig \
+                             src \
+
+COMPONENT_SRCDIRS :=  MediaHal \
+                      MediaHal/Codec \
+                      SystemSal \
+                      i2c_bus \
+                      src \
+                 
+
+
