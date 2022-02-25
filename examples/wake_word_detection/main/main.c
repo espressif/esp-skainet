7008c9bf1c4b9adf27417e97fa1f6d211e5b3bac
sxy <sunxiangyu@espressif.com>
2022-02-25 19:50:51 +0800

    update wakeword example

--- examples/wake_word_detection/main/main.c
+++ examples/wake_word_detection/main/main.c
@@ -20,6 +20,8 @@
 #include "esp_board_init.h"
 #include "driver/i2s.h"
 #include "model_path.h"
+#include "wav_encoder.h"
+#include "string.h"
 
 int detect_flag = 0;
 static esp_afe_sr_iface_t *afe_handle = NULL;
@@ -30,14 +32,40 @@ void feed_Task(void *arg)
     int audio_chunksize = afe_handle->get_feed_chunksize(afe_data);
     int nch = afe_handle->get_channel_num(afe_data);
     int feed_channel = esp_get_feed_channel();
-    int16_t *i2s_buff = malloc(audio_chunksize * sizeof(int16_t) * feed_channel);
+    int frame_size = audio_chunksize * sizeof(int16_t) * feed_channel;
+    int frame_num = 100;
+    int16_t *i2s_buff = malloc(frame_size);
+    int16_t *out_buff = malloc(frame_num*frame_size);
+    int out_buff_front = 0;
     assert(i2s_buff);
+    assert(out_buff);
     size_t bytes_read;
+    void * wav_encoder = NULL;
+    int idx=1;
+    char wav_file[128];
+    sprintf(wav_file, "/sdcard/clip_%d.wav", idx);
+    if (esp_sdcard_init("/sdcard", 10) == ESP_OK) {
+        wav_encoder=wav_encoder_open(wav_file, 16000, 16, 1);
+    }
 
     while (1) {
-        esp_get_feed_data(i2s_buff, audio_chunksize * sizeof(int16_t) * feed_channel);
+        esp_get_feed_data(i2s_buff, frame_size);
+
+        // copy i2s data into out_buff
+        // if wake word is detected, save out_buff into sdcard 
+        memcpy(out_buff + out_buff_front, i2s_buff, frame_size);
+        out_buff_front = (out_buff_front + frame_size)%(frame_num*frame_size);
 
         afe_handle->feed(afe_data, i2s_buff);
+
+        if (detect_flag && wav_encoder != NULL) {
+            wav_encoder_run(wav_encoder, out_buff + out_buff_front, frame_num*frame_size-out_buff_front);
+            wav_encoder_run(wav_encoder, out_buff, out_buff_front);
+            wav_encoder_close(wav_encoder);
+            idx ++;
+            sprintf(wav_file, "/sdcard/clip_%d.wav", idx);
+            wav_encoder = wav_encoder_open(wav_file, 16000, 16, 1);
+        }
     }
     afe_handle->destroy(afe_data);
     vTaskDelete(NULL);
@@ -57,6 +85,7 @@ void detect_Task(void *arg)
 
         if (res == AFE_FETCH_WWE_DETECTED) {
             printf("wakeword detected\n");
+            detect_flag = 1;
             printf("-----------LISTENING-----------\n");
         }
     }
