36db0a119604a9be8e47b1d43a6be65a08347947
xysun <sunxiangyu@espressif.com>
2024-08-05 20:14:26 +0800

    board: add esp32-p4-function-ev board

--- components/hardware_driver/boards/include/bsp_board.h
+++ components/hardware_driver/boards/include/bsp_board.h
@@ -45,6 +45,8 @@
     #include "esp32_s3_afe_raspberry_board.h"
 #elif CONFIG_ESP32_S3_BOX_3_BOARD
     #include "esp32_s3_box_3_board.h"
+#elif CONFIG_ESP32_P4_FUNCTION_EV_BOARD
+    #include "esp32_p4_function_ev_board.h"
 #else 
     #error "Please select type of dev board"
 #endif
