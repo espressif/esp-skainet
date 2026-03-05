e39adc95bfc0fedf270edfd953130b0a071aec7e
sunxiangyu <sunxiangyu@espressif.com>
2026-03-05 12:05:59 +0800

    feat: add wake word detection in player

--- examples/player/main/main.c
+++ examples/player/main/main.c
@@ -11,11 +11,21 @@
 #include "esp_skainet_player.h"
 #include "esp_console.h"
 
+#include "esp_wn_iface.h"
+#include "esp_wn_models.h"
+#include "esp_afe_sr_models.h"
+#include "esp_mn_iface.h"
+#include "esp_mn_models.h"
+#include "model_path.h"
+#include "string.h"
+
 #define MUSIC_PATH    "/sdcard/music/"
 #define RINGBUF_SIZE  (4096 * 4)
 
 static void *player = NULL;
-
+int detect_flag = 0;
+static esp_afe_sr_iface_t *afe_handle = NULL;
+static volatile int task_flag = 1;
 /* Console command handlers */
 static int cmd_play(int argc, char **argv)
 {
@@ -119,12 +129,78 @@ static void register_player_commands(void)
     ESP_ERROR_CHECK(esp_console_cmd_register(&vol_down_cmd));
 }
 
+
+void feed_Task(void *arg)
+{
+    esp_afe_sr_data_t *afe_data = arg;
+    int audio_chunksize = afe_handle->get_feed_chunksize(afe_data);
+    int nch = afe_handle->get_feed_channel_num(afe_data);
+    int feed_channel = esp_get_feed_channel();
+    assert(nch==feed_channel);
+    int16_t *i2s_buff = malloc(audio_chunksize * sizeof(int16_t) * feed_channel);
+    assert(i2s_buff);
+
+    while (task_flag) {
+        esp_get_feed_data(true, i2s_buff, audio_chunksize * sizeof(int16_t) * feed_channel);
+
+        afe_handle->feed(afe_data, i2s_buff);
+    }
+    if (i2s_buff) {
+        free(i2s_buff);
+        i2s_buff = NULL;
+    }
+    vTaskDelete(NULL);
+}
+
+void detect_Task(void *arg)
+{
+    esp_afe_sr_data_t *afe_data = arg;
+    int afe_chunksize = afe_handle->get_fetch_chunksize(afe_data);
+    int16_t *buff = malloc(afe_chunksize * sizeof(int16_t));
+    assert(buff);
+    printf("------------LISTENING------------\n");
+
+    while (task_flag) {
+        afe_fetch_result_t* res = afe_handle->fetch(afe_data); 
+        if (!res || res->ret_value == ESP_FAIL) {
+            printf("fetch error!\n");
+            break;
+        }
+        // printf("vad state: %d\n", res->vad_state);
+
+        if (res->wakeup_state == WAKENET_DETECTED) {
+            printf("\nDETECTED\n");
+        }
+    }
+    if (buff) {
+        free(buff);
+        buff = NULL;
+    }
+    vTaskDelete(NULL);
+}
+
+
 void app_main(void)
 {
     ESP_ERROR_CHECK(esp_board_init(16000, 1, 16));
     ESP_ERROR_CHECK(esp_sdcard_init("/sdcard", 10));
 
-    player = esp_skainet_player_create(RINGBUF_SIZE, 1, 2, 32, 16000);
+    srmodel_list_t *models = esp_srmodel_init("model");
+    if (models) {
+        for (int i=0; i<models->num; i++) {
+            if (strstr(models->model_name[i], ESP_WN_PREFIX) != NULL) {
+                printf("wakenet model in flash: %s\n", models->model_name[i]);
+            }
+        }
+    }
+
+    afe_config_t *afe_config = afe_config_init(esp_get_input_format(), models, AFE_TYPE_SR, AFE_MODE_LOW_COST);
+    afe_handle = esp_afe_handle_from_config(afe_config);
+    afe_config->vad_init=false;
+
+    esp_afe_sr_data_t *afe_data = afe_handle->create_from_config(afe_config);
+    afe_config_free(afe_config);
+    player = esp_skainet_player_create(RINGBUF_SIZE, 0, 2, 32, 16000);
     if (player == NULL) {
         printf("Failed to create player\n");
         return;
@@ -138,6 +214,8 @@ void app_main(void)
     printf("Playing WAV files from %s (use play/pause/stop/vol+/vol-)\n", MUSIC_PATH);
     esp_skainet_player_play(player, MUSIC_PATH);
     esp_skainet_player_pause(player);
+    xTaskCreatePinnedToCore(&feed_Task, "feed", 8 * 1024, (void*)afe_data, 5, NULL, 0);
+    xTaskCreatePinnedToCore(&detect_Task, "detect", 4 * 1024, (void*)afe_data, 5, NULL, 1);
 
     /* Console REPL for playback control */
     esp_console_repl_t *repl = NULL;
@@ -148,4 +226,5 @@ void app_main(void)
     esp_console_register_help_command();
     register_player_commands();
     ESP_ERROR_CHECK(esp_console_start_repl(repl));
+    
 }
